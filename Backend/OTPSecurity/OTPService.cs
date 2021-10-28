9e4e45cbc5817c03290162372a9a7b2d3e2d109c
Gabriel Habre <gabriel.habre@asteya.world>
2021-10-28 15:33:46 +0300

    all but 1 test added

--- Backend/OTPSecurity/OTPService.cs
+++ Backend/OTPSecurity/OTPService.cs
@@ -5,19 +5,51 @@ namespace OTPSecurity
 {
     public class OTPService
     {
-        public OTPService()
+        private string temp = null;
+        private DateTime last = DateTime.MinValue;
+        private readonly TimeSpan _delayBetweenGenerate;
+
+        public OTPService(TimeSpan delayBetweenGenerate = default)
         {
+            _delayBetweenGenerate = delayBetweenGenerate == default ? TimeSpan.FromMinutes(5) : delayBetweenGenerate;
         }
 
-        private const int otpLength = 5;
 
-        public bool ValidateToken(ValidateOTPModel model)
+        public bool ValidateOtp(ValidateOTPModel model)
         {
-            if (model.OTP.Length < otpLength)
+            return model.OTP == temp;
+        }
+
+        public string GenerateOtp(string email, string application)
+        {
+            if (string.IsNullOrWhiteSpace(email))
             {
-                return false;
+                throw new ArgumentException($"'{nameof(email)}' cannot be null or whitespace.", nameof(email));
             }
-            return true;
+
+            if (string.IsNullOrWhiteSpace(application))
+            {
+                throw new ArgumentException($"'{nameof(application)}' cannot be null or whitespace.", nameof(application));
+            }
+
+            if (!email.Contains("@"))
+            {
+                throw new ArgumentException($"Invalid '{nameof(email)}'");
+            }
+
+            if (DateTime.Now.Subtract(_delayBetweenGenerate) < last)
+            {
+                throw new InvalidOperationException("Not enough time has passed");
+            }
+            else
+            {
+                var rand = new Random();
+
+                temp = rand.Next(1000, 9999).ToString();
+                last = DateTime.Now;
+
+                return temp;
+            }
         }
     }
 }
--- Backend/OTPSecurityTests/UnitTest_Otp.cs
+++ Backend/OTPSecurityTests/UnitTest_Otp.cs
@@ -1,40 +1,168 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OTPSecurity;
 using OTPSecurity.Model;
+using System;
+using System.Threading.Tasks;
 
 namespace OTPSecurityTests
 {
     [TestClass]
     public class UnitTest_OTP
     {
+
         [TestMethod]
-        public void OTPLengthTest()
+        public void GenerateOtpTest()
         {
-            OTPService otpService = new OTPService();
-            var expectedOutput = true;
-            ValidateOTPModel model = new ValidateOTPModel();
-            Assert.AreEqual(expectedOutput, otpService.ValidateToken(model));
+            // Arrange
+            var otpService = new OTPService();
+
+            // Act
+            var otp = otpService.GenerateOtp("user@email.com", "12345");
+
+            // Assert
+            Assert.IsTrue(!string.IsNullOrEmpty(otp));
         }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GenerateOtpTestInvalidInputs()
+        {
+            // Arrange
+            var otpService = new OTPService();
+
+            // Act
+            var otp = otpService.GenerateOtp(null, "12345");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GenerateOtpTestInvalidEmail()
+        {
+            // Arrange
+            var otpService = new OTPService();
+
+            // Act
+            var otp = otpService.GenerateOtp("bla", "12345");
+        }
+
+
+
+
+        [TestMethod]
+        public void ValidateOtpWithCorrectCode()
+        {
+            // Arrange
+            var otpService = new OTPService();
+
+            var email = "user@email.com";
+
+            var otp = otpService.GenerateOtp(email, "12345");
+
+            var model = new ValidateOTPModel
+            {
+                Email = email,
+                OTP = otp
+            };
+
+            // Act
+            var validate = otpService.ValidateOtp(model);
+
+
+            // Assert
+            Assert.IsTrue(validate);
+        }
+
+
+
+        [TestMethod]
+        public void ValidateOtpWithInCorrectCode()
+        {
+            // Arrange
+            var otpService = new OTPService();
+
+            var email = "user@email.com";
+
+            var otp = otpService.GenerateOtp(email, "12345");
+
+            var model = new ValidateOTPModel
+            {
+                Email = email,
+                OTP = otp + "1"
+            };
+
+            // Act
+            var validate = otpService.ValidateOtp(model);
+
+
+            // Assert
+            Assert.IsFalse(validate);
+        }
+
+
 
         [TestMethod]
         public void EmailValidateTest()
         {
-            OTPService otpService = new OTPService();
+            var otpService = new OTPService();
             var expectedOutput = true;
-            ValidateOTPModel model = new ValidateOTPModel();
-            Assert.AreEqual(expectedOutput, otpService.ValidateToken(model));
+            var model = new ValidateOTPModel();
+            Assert.AreEqual(expectedOutput, otpService.ValidateOtp(model));
         }
 
         [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
         public void TimeDurationValidateTest()
         {
-            OTPService otpService = new OTPService();
-            var expectedOutput = true;
-            ValidateOTPModel model = new ValidateOTPModel();
-            Assert.AreEqual(expectedOutput, otpService.ValidateToken(model));
+            // Arrange
+            var otpService = new OTPService();
+
+            var email = "user@email.com";
+
+            var otp = otpService.GenerateOtp(email, "12345");
+
+            var model = new ValidateOTPModel
+            {
+                Email = email,
+                OTP = otp
+            };
+
+            // Act
+            var validate = otpService.ValidateOtp(model);
+
+
+            // Assert
+            otp = otpService.GenerateOtp(email, "12345");
+
         }
 
 
+        [TestMethod]
+        public async Task TimeDurationWaitValidateTest()
+        {
+            // Arrange
+            var otpService = new OTPService(TimeSpan.FromSeconds(1));
+
+            var email = "user@email.com";
+
+            var otp = otpService.GenerateOtp(email, "12345");
+
+            var model = new ValidateOTPModel
+            {
+                Email = email,
+                OTP = otp
+            };
+
+            // Act
+            var validate = otpService.ValidateOtp(model);
+
+            await Task.Delay(1000);
+
+            otp = otpService.GenerateOtp(email, "12345");
+
+            // Assert
+            Assert.IsTrue(!string.IsNullOrEmpty(otp));
+        }
 
 
 
